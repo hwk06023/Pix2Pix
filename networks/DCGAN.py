c8f074efae52ba6172d16a4e25cfc0303ca5dcd8
김현우 <hwk06023@gmail.com>
2019-01-26 14:07:04 +0900

    Revert "Revert "Model commit""
    This reverts commit 204a142f8a4df9d9790f3d18e7f307c64da7d5ea.

---
+++ networks/DCGAN.py
@@ -0,0 +1,30 @@
+from keras.layers import Input, Lambda
+from keras.models import Model
+
+
+def DCGAN(generator_model, discriminator_model, input_img_dim, patch_dim):
+    generator_input = Input(shape=input_img_dim, name="DCGAN_input")
+
+    # generated image model from the generator
+    generated_image = generator_model(generator_input)
+
+    h, w = input_img_dim[1:]
+    ph, pw = patch_dim
+
+    # chop the generated image into patches
+    list_row_idx = [(i * ph, (i + 1) * ph) for i in range(int(h / ph))]
+    list_col_idx = [(i * pw, (i + 1) * pw) for i in range(int(w / pw))]
+
+    list_gen_patch = []
+    for row_idx in list_row_idx:
+        for col_idx in list_col_idx:
+            x_patch = Lambda(lambda z: z[:, :, row_idx[0]:row_idx[1],
+                                       col_idx[0]:col_idx[1]], output_shape=input_img_dim)(generated_image)
+            list_gen_patch.append(x_patch)
+
+    # measure loss from patches of the image (not the actual image)
+    dcgan_output = discriminator_model(list_gen_patch)
+
+    # actually turn into keras model
+    dc_gan = Model(input=[generator_input], output=[generated_image, dcgan_output], name="DCGAN")
+    return dc_gan
